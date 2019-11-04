c60f442c1109685d88a5c2197ed35fd3e17eb779
Stéphan Paquet <stephanpaquet@gmail.com>
2019-11-04 06:38:29 -0500

    Add installation de vuetify

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+        <v-alert :value="true"  :dismissible="true" :elevation="3" type="success">
+            You have successfully installed vuetify 2 with Laravel 6 !
+        </v-alert>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "App.vue"
+    }
+</script>
+
